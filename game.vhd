39670b687fbb3fa5a4f743186a90fa5a9a128df0
Alpharceus <72083371+Alpharceus@users.noreply.github.com>
2023-12-02 21:09:33 -0700

    Update game.vhd

--- game.vhd
+++ game.vhd
@@ -106,28 +106,89 @@ architecture rtl of pong_graph_st is
    signal y_delta_reg, y_delta_next: unsigned(9 downto 0);
 
 -- ball movement can be pos or neg
-   constant BALL_V_P: unsigned(9 downto 0):= to_unsigned(2,10);
+   constant BALL_V_P: unsigned(9 downto 0):= to_unsigned(5,10);
    constant BALL_V_N: unsigned(9 downto 0):= unsigned(to_signed(-2,10));
 
 -- round ball image
    type rom_type is array(0 to 7) of std_logic_vector(0 to 7);
    constant BALL_ROM: rom_type:= (
-      "00000000",
-      "00000000",
-      "00000000", 
-      "00000000", 
-      "00000000", 
-      "00000000", 
-      "00000000",
-      "00000000");
-
+      "11100111",
+      "11100111",
+      "10111101", 
+      "10011001", 
+      "10011001", 
+      "10000001", 
+      "10000001",
+      "10000001");
    signal rom_addr, rom_col: unsigned(2 downto 0);
    signal rom_data: std_logic_vector(7 downto 0);
    signal rom_bit: std_logic;
+   
+   
+-- SECOND BALL SIGNAL DECLARATION
+    constant BALL2_SIZE: integer := 8;
+    signal ball2_x_l, ball2_x_r: unsigned(9 downto 0);
+    signal ball2_y_t, ball2_y_b: unsigned(9 downto 0);
+    
+    signal ball2_x_reg, ball2_x_next: unsigned(9 downto 0);
+    signal ball2_y_reg, ball2_y_next: unsigned(9 downto 0);
+    
+    signal x2_delta_reg, x2_delta_next: unsigned(9 downto 0);
+    signal y2_delta_reg, y2_delta_next: unsigned(9 downto 0);
+    
+    constant BALL2_V_P: unsigned(9 downto 0) := to_unsigned(2, 10);
+    constant BALL2_V_N: unsigned(9 downto 0) := unsigned(to_signed(-2, 10));
+
+    type rom_type2 is array(0 to 7) of std_logic_vector(0 to 7);
+    constant BALL2_ROM: rom_type2 := (
+       "11111111",
+       "11111111",
+       "00111000", 
+       "00111000", 
+       "00111000", 
+       "00111000", 
+       "00111000",
+       "00111000");
+    
+    signal rom2_addr, rom2_col: unsigned(2 downto 0);
+    signal rom2_data: std_logic_vector(7 downto 0);
+    signal rom2_bit: std_logic;
+
+
+-- THIRD BALL SIGNAL DECLARATION
+    constant BALL3_SIZE: integer := 8;
+    signal ball3_x_l, ball3_x_r: unsigned(9 downto 0);
+    signal ball3_y_t, ball3_y_b: unsigned(9 downto 0);
+    
+    signal ball3_x_reg, ball3_x_next: unsigned(9 downto 0);
+    signal ball3_y_reg, ball3_y_next: unsigned(9 downto 0);
+    
+    signal x3_delta_reg, x3_delta_next: unsigned(9 downto 0);
+    signal y3_delta_reg, y3_delta_next: unsigned(9 downto 0);
+    
+    constant BALL3_V_P: unsigned(9 downto 0) := to_unsigned(3, 10);
+    constant BALL3_V_N: unsigned(9 downto 0) := unsigned(to_signed(-2, 10));
+
+    type rom_type3 is array(0 to 7) of std_logic_vector(0 to 7);
+    constant BALL3_ROM: rom_type3 := (
+       "11111111",
+       "11100111",
+       "11000011", 
+       "10000001", 
+       "10000001", 
+       "11000011", 
+       "11100111",
+       "11111111");
+    
+    signal rom3_addr, rom3_col: unsigned(2 downto 0);
+    signal rom3_data: std_logic_vector(7 downto 0);
+    signal rom3_bit: std_logic;
+
+
 
 -- object output signals -- new signal to indicate if scan coord is within ball
-   signal wall_on, wall2_on, bar_on, sq_ball_on, rd_ball_on,trust_on,road1_on, road2_on, road3_on, road4_on, road5_on, road6_on, road7_on, road8_on: std_logic;
-   signal wall_rgb, wall2_rgb, bar_rgb, ball_rgb, trust_rgb, road1_rgb, road2_rgb, road3_rgb, road4_rgb, road5_rgb, road6_rgb, road7_rgb, road8_rgb: std_logic_vector(2 downto 0);
+   signal wall_on, wall2_on, bar_on, sq_ball_on, rd_ball_on,sq_ball2_on, rd_ball2_on,sq_ball3_on, rd_ball3_on,trust_on,road1_on, road2_on, road3_on, road4_on, road5_on, road6_on, road7_on, road8_on: std_logic;
+   signal wall_rgb, wall2_rgb, bar_rgb, ball_rgb, ball2_rgb,ball3_rgb,trust_rgb, road1_rgb, road2_rgb, road3_rgb, road4_rgb, road5_rgb, road6_rgb, road7_rgb, road8_rgb: std_logic_vector(2 downto 0);
 
 -- ======================================================================================================
    begin
@@ -136,16 +197,41 @@ architecture rtl of pong_graph_st is
       begin
       if (reset = '1') then
          bar_x_reg <= ("0100011100");
-         ball_x_reg <= (others => '0');
+         
+         ball_x_reg <= ("0100111100");
          ball_y_reg <= (others => '0');
-         x_delta_reg <= ("0000000100");
-         y_delta_reg <= ("0000000100");
+         x_delta_reg <= ("0000000000");
+         y_delta_reg <= BALL_V_P;
+         
+         
+         ball2_x_reg <= ("1000010001");
+         ball2_y_reg <= (others => '0');
+         x2_delta_reg <= ("0000000000");
+         y2_delta_reg <= BALL2_V_P;
+         
+         ball3_x_reg <= ("0001100111");
+         ball3_y_reg <= (others => '0');
+         x3_delta_reg <= ("0000000000");
+         y3_delta_reg <= BALL3_V_P;
+         
       elsif (clk'event and clk = '1') then
          bar_x_reg <= bar_x_next;
+         
          ball_x_reg <= ball_x_next;
          ball_y_reg <= ball_y_next;
          x_delta_reg <= x_delta_next;
          y_delta_reg <= y_delta_next;
+         
+         ball2_x_reg <= ball2_x_next;
+         ball2_y_reg <= ball2_y_next;
+         x2_delta_reg <= x2_delta_next;
+         y2_delta_reg <= y2_delta_next;
+         
+         
+         ball3_x_reg <= ball3_x_next;
+         ball3_y_reg <= ball3_y_next;
+         x3_delta_reg <= x3_delta_next;
+         y3_delta_reg <= y3_delta_next;
       end if;
    end process;
 
@@ -240,8 +326,8 @@ architecture rtl of pong_graph_st is
    rom_bit <= rom_data(to_integer(rom_col));
 
 -- Turn ball on only if within square and the ROM bit is 1.
-   rd_ball_on <= '1' when (sq_ball_on = '1') and (rom_bit = '1') else '0';
-   ball_rgb <= "000"; -- red
+   rd_ball_on <= '1' when (sq_ball_on = '1') and (rom_bit = '1') and (ball_y_b <= bar_y_t) else '0';
+   ball_rgb <= "100"; -- red
 
 -- Update the ball position 60 times per second.
    ball_x_next <= ball_x_reg + x_delta_reg when refr_tick = '1' else ball_x_reg;
@@ -257,9 +343,9 @@ architecture rtl of pong_graph_st is
       if ( ball_y_t < 1 ) then 
          y_delta_next <= BALL_V_P;
 
--- Reached bottom, make negative
-      elsif (ball_y_b > (MAX_Y - 1)) then 
-         y_delta_next <= BALL_V_N;
+--Reached bottom, make negative
+     --elsif (ball_y_b > (MAX_Y - 1)) then 
+       --  x_delta_next <= "0011010010";
 
 -- Reach wall, bounce back
       elsif (ball_x_l <= WALL_X_R ) then 
@@ -274,10 +360,95 @@ architecture rtl of pong_graph_st is
          end if;
       end if;
    end process;
+   
+ --+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+ 
+    ball2_x_l <= ball2_x_reg;
+    ball2_y_t <= ball2_y_reg;
+    ball2_x_r <= ball2_x_l + BALL2_SIZE - 1;
+    ball2_y_b <= ball2_y_t + BALL2_SIZE - 1;
+    
+    sq_ball2_on <= '1' when (ball2_x_l <= pix_x) and (pix_x <= ball2_x_r) and (ball2_y_t <= pix_y) and (pix_y <= ball2_y_b) else '0';
+    
+    rom2_addr <= pix_y(2 downto 0) - ball2_y_t(2 downto 0);
+    rom2_col <= pix_x(2 downto 0) - ball2_x_l(2 downto 0);
+    
+    rom2_data <= BALL2_ROM(to_integer(rom2_addr));
+    rom2_bit <= rom2_data(to_integer(rom2_col));
+    
+    rd_ball2_on <= '1' when (sq_ball2_on = '1') and (rom2_bit = '1') and (ball2_y_b <= bar_y_t) else '0';
+    ball2_rgb <= "010";
+    
+    ball2_x_next <= ball2_x_reg + x2_delta_reg when refr_tick = '1' else ball2_x_reg;
+    ball2_y_next <= ball2_y_reg + y2_delta_reg when refr_tick = '1' else ball2_y_reg;
+    
+    process(x2_delta_reg, y2_delta_reg, ball2_y_t, ball2_x_l, ball2_x_r, ball2_y_t, ball2_y_b, bar_x_l, bar_x_r)
+    begin
+       x2_delta_next <= x2_delta_reg;
+       y2_delta_next <= y2_delta_reg;
+    
+       if (ball2_y_t < 1) then 
+          y2_delta_next <= BALL2_V_P;
+       --elsif (ball2_y_b > (MAX_Y - 1)) then 
+          --y2_delta_next <= BALL2_V_N;
+       elsif (ball2_x_l <= WALL_X_R) then 
+          x2_delta_next <= BALL2_V_P; 
+       elsif ((BAR_Y_B <= ball2_x_r) and (ball2_x_r <= BAR_Y_B)) then
+          if ((bar_x_r <= ball2_y_b) and (ball2_y_t <= bar_x_r)) then
+             x2_delta_next <= BALL2_V_N; 
+          end if;
+       end if;
+    end process;
+
+ 
+ --+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+ 
+ 
+  --+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+ 
+    ball3_x_l <= ball3_x_reg;
+    ball3_y_t <= ball3_y_reg;
+    ball3_x_r <= ball3_x_l + BALL3_SIZE - 1;
+    ball3_y_b <= ball3_y_t + BALL3_SIZE - 1;
+    
+    sq_ball3_on <= '1' when (ball3_x_l <= pix_x) and (pix_x <= ball3_x_r) and (ball3_y_t <= pix_y) and (pix_y <= ball3_y_b) else '0';
+    
+    rom3_addr <= pix_y(2 downto 0) - ball3_y_t(2 downto 0);
+    rom3_col <= pix_x(2 downto 0) - ball3_x_l(2 downto 0);
+    
+    rom3_data <= BALL3_ROM(to_integer(rom3_addr));
+    rom3_bit <= rom3_data(to_integer(rom3_col));
+    
+    rd_ball3_on <= '1' when (sq_ball3_on = '1') and (rom3_bit = '1') and (ball3_y_b <= bar_y_t) else '0';
+    ball3_rgb <= "001";
+    
+    ball3_x_next <= ball3_x_reg + x3_delta_reg when refr_tick = '1' else ball3_x_reg;
+    ball3_y_next <= ball3_y_reg + y3_delta_reg when refr_tick = '1' else ball3_y_reg;
+    
+    process(x3_delta_reg, y3_delta_reg, ball3_y_t, ball3_x_l, ball3_x_r, ball3_y_t, ball3_y_b, bar_x_l, bar_x_r)
+    begin
+       x3_delta_next <= x3_delta_reg;
+       y3_delta_next <= y3_delta_reg;
+    
+       if (ball3_y_t < 1) then 
+          y3_delta_next <= BALL3_V_P;
+       --elsif (ball2_y_b > (MAX_Y - 1)) then 
+          --y2_delta_next <= BALL2_V_N;
+       elsif (ball2_x_l <= WALL_X_R) then 
+          --x2_delta_next <= BALL2_V_P; 
+       elsif ((BAR_Y_B <= ball3_x_r) and (ball3_x_r <= BAR_Y_B)) then
+          if ((bar_x_r <= ball3_y_b) and (ball3_y_t <= bar_x_r)) then
+             x3_delta_next <= BALL3_V_N; 
+          end if;
+       end if;
+    end process;
+
+ 
+ --+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 -- ======================================================================================================
 -- turn on the appropriate color depending on the current pixel position.
-   process (video_on, wall_on, wall2_on, bar_on, rd_ball_on,trust_on, wall_rgb, wall2_rgb, bar_rgb, ball_rgb,trust_rgb,road1_on, road2_on, road3_on, road4_on, road5_on, road6_on, road7_on, road8_on,road1_rgb, road2_rgb, road3_rgb, road4_rgb, road5_rgb, road6_rgb,road7_rgb, road8_rgb)
+   process (video_on, wall_on, wall2_on, bar_on, rd_ball_on, rd_ball2_on,rd_ball3_on,trust_on, wall_rgb, wall2_rgb, bar_rgb, ball_rgb,trust_rgb,road1_on, road2_on, road3_on, road4_on, road5_on, road6_on, road7_on, road8_on,road1_rgb, road2_rgb, road3_rgb, road4_rgb, road5_rgb, road6_rgb,road7_rgb, road8_rgb)
       begin
       if (video_on = '0') then
          graph_rgb <= "000"; -- blank
@@ -290,6 +461,12 @@ architecture rtl of pong_graph_st is
             graph_rgb <= trust_rgb;
          elsif (bar_on = '1') then
             graph_rgb <= bar_rgb;
+         elsif (rd_ball_on = '1') then
+            graph_rgb <= ball_rgb;
+         elsif (rd_ball2_on = '1') then
+            graph_rgb <= ball2_rgb;   
+         elsif (rd_ball3_on = '1') then
+            graph_rgb <= ball3_rgb;
          elsif (road1_on = '1') then
             graph_rgb <= road1_rgb;
          elsif (road2_on = '1') then
@@ -306,8 +483,6 @@ architecture rtl of pong_graph_st is
             graph_rgb <= road7_rgb;
          elsif (road8_on = '1') then
             graph_rgb <= road8_rgb;
-         elsif (rd_ball_on = '1') then
-            graph_rgb <= ball_rgb;
          else
             graph_rgb <= "111"; -- yellow bkgnd
          end if;
